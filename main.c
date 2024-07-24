d3c0ea991165735dc4bc673875a5645b63be2553
George Marshall <george@japanmarshalls.com>
2024-07-24 12:04:13 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
-printf("Hello World\n");
+#include <stdlib.h>
+#include <strings.h>
+
+// create a structure that holds question/answer pairs
+
+// weighted graph adt implementation!
+
+// create a function that reads the file and creates q/a pairs based on that (and adds to the graph!)
+
+int main(){
+  
+  return 0;
+}
